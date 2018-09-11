b16aeaa2f790301eac3d79a9172e6ea479acd23a
tigersean <sean.gong@gmail.com>
2018-09-11 16:52:48 +0800

    commit

--- simpleQuant/Util/Util_error.py
+++ simpleQuant/Util/Util_error.py
@@ -28,30 +28,30 @@
 
 class QAError_fetch_data(RuntimeError):
     def __init__(self, res):
-        RuntimeError.__init__(self, 'QA FETCH DATA ERROR', res)
+        RuntimeError.__init__(self, 'FETCH DATA ERROR', res)
 
 
 class QAError_no_data_in_database(RuntimeError):
     def __init__(self, res):
-        RuntimeError.__init__(self, 'QA FETCH NO DATA ERROR', res)
+        RuntimeError.__init__(self, 'FETCH NO DATA ERROR', res)
 
 
 class QAError_crawl_data_web(RuntimeError):
     def __init__(self, res):
-        RuntimeError.__init__(self, 'QA CRAWLER ERROR', res)
+        RuntimeError.__init__(self, 'CRAWLER ERROR', res)
 
 
 class QAError_database_connection(RuntimeError):
     def __init__(self, res):
-        RuntimeError.__init__(self, 'QA DATABASE CONNECTION ERROR', res)
+        RuntimeError.__init__(self, 'DATABASE CONNECTION ERROR', res)
 
 
 class QAError_web_connection(RuntimeError):
     def __init__(self, res):
-        RuntimeError.__init__(self, 'QA WEB CONNECTION ERROR', res)
+        RuntimeError.__init__(self, 'WEB CONNECTION ERROR', res)
 
 
 class QAError_market_enging_down(RuntimeError):
     def __init__(self, res):
-        RuntimeError.__init__(self, 'QA MARKET ENGING DOWN ERROR', res)
+        RuntimeError.__init__(self, 'MARKET ENGING DOWN ERROR', res)
 
